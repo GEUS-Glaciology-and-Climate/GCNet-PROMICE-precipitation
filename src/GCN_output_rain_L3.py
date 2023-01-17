48c4b15413b96a08f7cee9272f87d7b57f799abc
Jason Box <jason@Jasons-MacBook-Pro.local>
2023-01-17 20:31:45 +0100

    cleaner code

--- src/GCN_output_rain_L3.py
+++ src/GCN_output_rain_L3.py
@@ -12,20 +12,12 @@ to evaluate issues in precipitation data
 """
 
 
-from datetime import datetime
 import pandas as pd
 import os
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
 import numpy as np
-from numpy.polynomial.polynomial import polyfit
 from datetime import date
-import geopandas as gpd
-from pyproj import Proj, transform
-# from matplotlib import colors
-from matplotlib.pyplot import figure
-from PIL import Image
-from pandas import Timestamp
 
 th=2 # line thickness
 formatx='{x:,.3f}' ; fs=16
@@ -45,57 +37,27 @@ plt.rcParams['figure.figsize'] = 12, 20
 
 if os.getlogin() == 'jason':
     base_path = '/Users/jason/Dropbox/AWS/GCNet-PROMICE-precipitation/'
-    aws_data_path='/Users/jason/Dropbox/AWS/aws-l3/level_3/'
     aws_data_path='/Users/jason/0_dat/AWS/aws-l3/level_3/'
 
 os.chdir(base_path)
 
-today = date.today()
-versionx= today.strftime('%Y-%m-%d')
-
-# meta = pd.read_excel('/Users/jason/Dropbox/AWS/CARRA-TU_GEUS/metadata/IMEI_numbers_station_2021-10-27.xlsx')
 
 meta = pd.read_csv('/Users/jason/0_dat/AWS/aws-l3/AWS_station_locations.csv')
-
 meta = meta.rename({'stid': 'name'}, axis=1)
-# fn='/Users/jason/Dropbox/AWS/PROMICE/ancil/PROMICE_info_all_2017-2018.csv'
-# meta=pd.read_csv(fn)
 print(meta.columns)
-
 names=meta.name
-
-do_plot=1
-plt_elev_fit=0
-
-# def pltovar(x,y,units,varname):
-#     # ax.plot(y,'-o',label=varname)
-#     units=units
-#     varname=varname
-    
-#     v=np.where(y.values==np.nanmax(y.values))
-#     print(x[v][0],y[v][0])
-#     # v=y==np.nanmax(y)
-
-#     # vv=[i for i, x in enumerate(v) if x]
-#     # first_case=pd.to_datetime(x[vv[0]])
-    
-#     # s = pd.Series({Timestamp(str(first_case)): np.nanmax(y)})
-#     # s.plot(ax=ax)
-#     # ax.text(s.index[0], s.iloc[0], 'max: '+" %.1f" % np.nanmax(y)+' '+units)
-#     # print(s.index[0], s.iloc[0])
-    #
-
 names=names[names!='Roof_PROMICE']
 names=names[names!='Roof_GEUS']
 
-do_plot=1
 
-if do_plot:fig, ax = plt.subplots(figsize=(10,10))
+do_plots=1
+
+if do_plots:fig, ax = plt.subplots(figsize=(10,10))
 
 for i,name in enumerate(names):
     if i>=0:
     # if name=='NUK_Uv3':
-    # if name=='QAS_Uv3': # not added yet
+    # if name=='QAS_Uv3'
     # if name=='QAS_L':
     # if name=='CP1':
     # if name=='DY2':
@@ -116,19 +78,11 @@ for i,name in enumerate(names):
         fn=aws_data_path+site+'/'+site+'_day.csv'
         df=pd.read_csv(fn)        
         print(df.columns)
-        # df.t_u
-        # position
-        n=20
-        lat=np.nanmean(df.gps_lat[-n:])
-        lon=-np.nanmean(df.gps_lon[-n:])
-        elev=np.nanmean(df.gps_alt[-n:])
-        print(site+", N %.3f" %lat+'°,W %.3f' %lon+', %.0f' %elev+' m')
         
         df.index = pd.to_datetime(df.time)
         
         iyear=2020
         fyear=2022
-        # iyear=2022
         n_years=fyear-iyear+1
         
         issues_site=[]
@@ -194,7 +148,7 @@ for i,name in enumerate(names):
                         x.to_csv('./output/'+site+'_'+str(year)+'.csv',
                                  columns=varnams,index=None, float_format="%.1f")
                         
-                        if do_plot:
+                        if do_plots:
                             x.index = pd.to_datetime(x.time)
                             fig, ax = plt.subplots(figsize=(10,8))
                             ax.plot(x.tp_u, drawstyle='steps-mid',label='precip. rate from precip_u_cor')
